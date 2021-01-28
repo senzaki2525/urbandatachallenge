980767342474dc8a15c5cff50db91d914f72e11e
Ryosuke Senzaki <ase18410014@wiz.ac.jp>
2021-01-28 14:02:37 +0900

    first commit

---
+++ wwwroot/js/map.js
@@ -0,0 +1,21 @@
+﻿var map;
+var marker;
+var center = {
+    lat: 34.7019399, // 緯度
+    lng: 135.51002519999997 // 経度
+};
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('gmap'), { // #sampleに地図を埋め込む
+        center: center,
+        zoom: 19 // 地図のズームを指定
+    });
+
+
+    marker = new google.maps.Marker({ // マーカーの追加
+        position: center, // マーカーを立てる位置を指定
+        map: map // マーカーを立てる地図を指定
+    });
+}
+
+
